a15a7b6d220328dda96bb7ea21c6856f9e3236d9
Adrian Wałach <aw055966@student.ath.edu.pl>
2022-11-30 17:59:55 +0100

    Changed endpoint

--- Program.cs
+++ Program.cs
@@ -1,16 +1,44 @@
 using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "Origins",
+        policy =>
+        {
+            policy.WithOrigins("*");
+        });
+});
 var app = builder.Build();
 
 app.MapGet("/getwaypoints", () => {
     var waypoints = new List<Waypoint>();
 
-    waypoints.Add(new Waypoint("Waypoint 1", 49.81693887423546f, 19.013345700074744f));
-    waypoints.Add(new Waypoint("Waypoint 2", 49.81813459091276f, 19.01493052434592f));
-    waypoints.Add(new Waypoint("Waypoint 3", 49.81318871942243f, 19.01085903690916f));
+    waypoints.Add(new Waypoint
+    {
+        Name = "Waypoint 1",
+        Latitude = 49.81693887423546f, 
+        Longitude = 19.013345700074744f,
+        Task = "Zrób 10 pajacyków"
+    });
+    waypoints.Add(new Waypoint
+    {
+        Name = "Waypoint 2",
+        Latitude = 49.81813459091276f, 
+        Longitude = 19.01493052434592f,
+        Task = "Zrób 10 brzuszków"
+    });
+    waypoints.Add(new Waypoint
+    {
+        Name = "Waypoint 3",
+        Latitude = 49.81318871942243f, 
+        Longitude = 19.01085903690916f,
+        Task = "Zrób 10 pajacyków"
+    });
 
     return JsonSerializer.Serialize(waypoints);
 });
+
+app.UseCors();
 
 app.Run();
--- Waypoint.cs
+++ Waypoint.cs
@@ -3,10 +3,5 @@ class Waypoint
     public string Name { get; set; }
     public float Latitude { get; set; }
     public float Longitude { get; set; }
-    public Waypoint(string name, float latitude, float longitude)
-    {
-        Name = name;
-        Latitude = latitude;
-        Longitude = longitude;
-    }
+    public string Task { get; set; }
 }
